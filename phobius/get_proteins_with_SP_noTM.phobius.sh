c8fe40f59f194ec8b70263c69149c5ef2cdd0216
Thaís Dal'Sasso <thaisdalsasso@gmail.com>
2025-03-12 09:44:15 +0100

    Add files via upload

---
+++ phobius/get_proteins_with_SP_noTM.phobius.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Iterate over each *.phobius.short.out file in the current directory
+for phobius_file in *.phobius.short.out; do
+
+    # Set the base name
+    base_name=$(basename "$phobius_file" | cut -d. -f1)
+
+    #  Check conditions and extract IDs
+    output_file="${base_name}.noTM.SP.phobius.ids"
+    awk '!/^SEQENCE/ && $2 == 0 && $3 == "Y" {print $1}' "$phobius_file" > "$output_file"
+
+    echo "Protein IDs with no transmembrane domains and signal peptide in $base_name have been saved to $output_file"
+done
+
---
+++ phobius/phobius.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#BATCH --job-name=phobius #Give your job a name.
+
+#SBATCH --nodes=1 #Only increase for openmpi jobs.
+#SBATCH --ntasks=1 #e.g if set to 2, could run two softwares in the script at the same time.
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10 #Multithreading.
+#SBATCH --time=72:00:00 #Time for a job to run given as hh:mm:ss.
+#SBATCH --mem=10G #Total Memory per node to use for the job
+#SBATCH --error=job.%J.err #Std Error write standard error to this file
+#SBATCH --output=job.%J.out #Std Out write standard output to this file
+#SBATCH --mail-type=FAIL #Notify user by email when certain event types occur (BEGIN, END, FAIL, REQUEUE)
+#SBATCH --mail-user=dalsasso@evolbio.mpg.de #Email for notifications from previous line
+#SBATCH --partition=standard #Request a specific partition for the resource allocation.
+
+
+##########################
+# Module load:
+#module load $MODULE_NAME 
+
+#########################
+# Run commands from here:
+
+# Set the directory containing your protein files
+protein_dir="/home/dalsasso/data/References/proteomes/"
+
+# Set the output directory for Phobius results
+phobius_output_dir="/home/dalsasso/annotations/phobius/"
+
+# Iterate over protein files in the directory
+for protein_file in "$protein_dir"*no_stop_codon.fa; do
+   
+    # Get the base name (before the first "_") of the file
+    base_name=$(basename "$protein_file" | cut -d_ -f1)
+
+    # Run Phobius
+    perl /home/dalsasso/softwares/phobius/phobius.pl -short "$protein_file" > "${phobius_output_dir}${base_name}.phobius.short.out"
+
+done
+
+
