337a4846cf0e56be1102267b1ffd5f84578fa3e7
Thaís Dal'Sasso <thaisdalsasso@gmail.com>
2025-03-12 09:57:40 +0100

    Update and rename get_proteins_with_noTM.tmhmm.sh to 2_get_proteins_with_noTM.tmhmm.sh

--- tmhmm2/2_get_proteins_with_noTM.tmhmm.sh
+++ tmhmm2/2_get_proteins_with_noTM.tmhmm.sh
@@ -2,10 +2,10 @@
 
 #Iterate over each *.tmhmm.out file in the current directory
 for tmhmm_file in *.tmhmm.out; do
-    #Set the base name
+
     base_name=$(basename "$tmhmm_file" | cut -d. -f1)
 
-    #Check for the condition and extract gene names
+    #Check for the conditions
     output_file="${base_name}.noTM.tmhmm.ids"
     awk '/Number of predicted TMHs:  0/ {print $2}' "$tmhmm_file" > "$output_file"
 
