b421a70b85602f81b4d9374bc030b64d74295563
Thaís Dal'Sasso <thaisdalsasso@gmail.com>
2025-03-12 09:33:07 +0100

    Update run_lipases.sh

--- lipases/run_lipases.sh
+++ lipases/run_lipases.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for i in ../fastas4anno/*prot_renamedIDs.fa
+
+proteome_dir="/Users/dalsasso/Desktop/Posdoc/CAU/data/References/proteomes"
+lipases_dir="/Users/dalsasso/Desktop/Posdoc/CAU/data/databases/lipases_db"
+
+
+
+for i in $proteome_dir/*_no_stop_codon.fa
 do
-    base_filename=$(basename "$i")
-    hmmscan --domtblout "$base_filename.hmmscan.lipases.dom.tbl" /workspace/thais/lipases_db/dbLED.txt "$i" > "./$base_filename.hmmscan.lipases.dom.out"
+    base_filename=$(basename "$i" "_no_stop_codon.fa")
+    hmmscan --domtblout "$base_filename.hmmscan.lipases.dom.tbl" $lipases_dir/dbLED.txt "$i" > "./$base_filename.hmmscan.lipases.dom.out"
     
     python3 hmmscan-parser.py "./$base_filename.hmmscan.lipases.dom.tbl" > "./$base_filename.hmmscan.lipases.parsed"
 
 done
-
