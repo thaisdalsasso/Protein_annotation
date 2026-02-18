ac5878c4af40153152b65b95e0ea12fa45e50ad7
Thaís Dal'Sasso <thaisdalsasso@gmail.com>
2026-02-18 11:17:26 +0100

    Update
    Removed confirmation message for saved protein IDs.

--- signalp6/2_get_proteins_with_SP.signalp6.sh
+++ signalp6/2_get_proteins_with_SP.signalp6.sh
@@ -9,7 +9,6 @@ for subfolder in */; do
 
     if [ -e "$gff_file" ]; then
         awk '!/^#/ {print $1}' "$gff_file" > "$output_file"
-        echo "Protein IDs with signal peptide in $base_name have been saved to $output_file"
     else
         echo "Error: $gff_file not found in $base_name"
     fi
